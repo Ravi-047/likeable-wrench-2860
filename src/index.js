c931ebd978c66f547a3b1681b19613de58e287eb
kaushalragini <ragini11.93@gmail.com>
2022-12-14 23:18:29 +0530

    merging changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 
-import AuthContextProveder from "./Context/AuthContesxt"
+import AuthContextProvider from "./Context/AuthContesxt";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
     <ChakraProvider>
-      <AuthContextProveder>
+      <AuthContextProvider>
         <App />
-      </AuthContextProveder>
+      </AuthContextProvider>
     </ChakraProvider>
   </BrowserRouter>
   // </React.StrictMode>
