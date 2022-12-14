28b5a729cf38422add39285b1daa7fb0269181de
kaushalragini <ragini11.93@gmail.com>
2022-12-14 23:24:55 +0530

    merged changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import AuthContextProvider from "./Context/AuthContesxt";
 
 import AuthContextProvider from "./Context/AuthContext";
 
